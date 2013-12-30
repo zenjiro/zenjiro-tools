8fa7281df4b30664c8df22df6f43c20aecfa50d8
zenjiro <zenjiro0123@gmail.com>
2013-12-30 15:48:37 +0900

    バウンスメールをロギングしてみたけど何も届かない。
    #3 の修整中。mixiから何か届くのを期待したけどダメだった。届いてないのか処理できていないのか分からない。

---
+++ zenjiro tools/src/zenjiro/BounceHandlerServlet.java
@@ -0,0 +1,37 @@
+package zenjiro;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.mail.BounceNotification;
+import com.google.appengine.api.mail.BounceNotificationParser;
+
+public class BounceHandlerServlet extends HttpServlet {
+	@Override
+	public void doPost(final HttpServletRequest req,
+			final HttpServletResponse resp) throws IOException {
+		try {
+			final BounceNotification bounce = BounceNotificationParser
+					.parse(req);
+			Logger.getAnonymousLogger()
+					.log(Level.WARNING,
+							"from: {0}, to: {1}, subject: {2}, text: {3}, from: {4}, to: {5}, subject: {6}, text: {7}",
+							new Object[] { bounce.getOriginal().getFrom(),
+									bounce.getOriginal().getTo(),
+									bounce.getOriginal().getSubject(),
+									bounce.getOriginal().getText(),
+									bounce.getNotification().getFrom(),
+									bounce.getNotification().getTo(),
+									bounce.getNotification().getSubject(),
+									bounce.getNotification().getText() });
+		} catch (final MessagingException e) {
+			throw new IOException(e);
+		}
+	}
+}
