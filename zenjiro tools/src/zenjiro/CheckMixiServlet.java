d66fa165480abf03a488d80c9327ebc171251dce
zenjiro <zenjiro0123@gmail.com>
2014-01-01 16:27:17 +0900

    イベントのタイトルとURLをロギング

--- zenjiro tools/src/zenjiro/CheckMixiServlet.java
+++ zenjiro tools/src/zenjiro/CheckMixiServlet.java
@@ -7,6 +7,8 @@ import java.net.URL;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
 import javax.servlet.ServletException;
@@ -19,17 +21,24 @@ public class CheckMixiServlet extends HttpServlet {
 	protected void doGet(final HttpServletRequest req,
 			final HttpServletResponse resp) throws ServletException,
 			IOException {
-		final URL url = new URL("http://mixi.jp/new_bbs.pl");
-		final HttpURLConnection connection = (HttpURLConnection) url
-				.openConnection();
+		final HttpURLConnection connection = (HttpURLConnection) new URL(
+				"http://mixi.jp/new_bbs.pl").openConnection();
 		connection.setRequestMethod("GET");
 		connection.setRequestProperty("Accept-Encoding", "gzip");
 		connection.setRequestProperty("Cookie", Const.MIXI_COOKIE);
 		final Scanner scanner = new Scanner(new InputStreamReader(
 				new GZIPInputStream(connection.getInputStream()), "EUC-JP"));
 		while (scanner.hasNextLine()) {
-			final String line = scanner.nextLine();
-			Logger.getAnonymousLogger().log(Level.INFO, line);
+			final Matcher matcher = Pattern
+					.compile(
+							"<p class=\"title iconEvent\"><a href=\"(http://mixi.jp/view_event.pl\\?comment_count=[0-9]+&comm_id=[0-9]+&id=[0-9]+)\">(.+)</a></p>")
+					.matcher(scanner.nextLine().trim());
+			if (matcher.matches()) {
+				final String url = matcher.group(1);
+				final String title = matcher.group(2);
+				Logger.getAnonymousLogger().log(Level.INFO,
+						"title: {0}, url: {1}", new String[] { title, url });
+			}
 		}
 		scanner.close();
 	}
