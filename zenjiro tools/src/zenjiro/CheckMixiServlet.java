09d70cfcab5d0ef87ace0c8a9d9cbee346e621af
zenjiro <zenjiro0123@gmail.com>
2014-01-01 16:05:02 +0900

    mixiの参加コミュニティの最新書き込みを読み込んでそのままロギング
    #5 の作業中。

--- zenjiro tools/src/zenjiro/CheckMixiServlet.java
+++ zenjiro tools/src/zenjiro/CheckMixiServlet.java
@@ -1,8 +1,13 @@
 package zenjiro;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +19,18 @@ public class CheckMixiServlet extends HttpServlet {
 	protected void doGet(final HttpServletRequest req,
 			final HttpServletResponse resp) throws ServletException,
 			IOException {
-		Logger.getAnonymousLogger().log(Level.INFO, "わーい");
+		final URL url = new URL("http://mixi.jp/new_bbs.pl");
+		final HttpURLConnection connection = (HttpURLConnection) url
+				.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setRequestProperty("Accept-Encoding", "gzip");
+		connection.setRequestProperty("Cookie", Const.MIXI_COOKIE);
+		final Scanner scanner = new Scanner(new InputStreamReader(
+				new GZIPInputStream(connection.getInputStream()), "EUC-JP"));
+		while (scanner.hasNextLine()) {
+			final String line = scanner.nextLine();
+			Logger.getAnonymousLogger().log(Level.INFO, line);
+		}
+		scanner.close();
 	}
 }
